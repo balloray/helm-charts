2ffd348e7f4a9d5b48ed5394b2f399382bad74f1
None <balloray@bastion-fuchicorp-com.us-east1-b.c.geometric-sled-282503.internal>
2022-11-08 17:01:57 -0600

    x

--- concourse.tf
+++ concourse.tf
@@ -1,75 +1,14 @@
-# locals {
-#   required_values = {
-#     # Deployment endpoint for ingress
-#     deployment_endpoint = lower(var.deployment_endpoint)
+## The remote chart deployment 
+resource "helm_release" "helm_deployment" {
+  name                = var.deployment_name
+  namespace           = var.deployment_environment
+  chart               = var.deployment_path
+  timeout             = var.timeout
+  recreate_pods       = var.recreate_pods
+  version             = var.release_version
+  repository          = var.chart_repo
 
-#     ## The release name will be 
-#     deployment_name = lower(var.deployment_name)
-#   }
-
-#   # When all requred values all deffined then also will incloude users values
-#   template_all_values = merge(local.required_values, var.template_custom_vars)
-#   timeout             = var.timeout
-#   recreate_pods       = var.recreate_pods
-#   trigger             = var.trigger == "UUID" ? uuid() : var.trigger
-# }
-
-
-# ## The remote chart deployment 
-# resource "helm_release" "helm_remote_deployment" {
-#   count         = var.enabled == true && var.remote_chart == true ? 1 : 0
-#   name          = var.deployment_name
-#   namespace     = var.deployment_environment
-#   chart         = var.deployment_path
-#   timeout       = local.timeout
-#   recreate_pods = local.recreate_pods
-#   version       = var.release_version
-#   repository    = var.chart_repo
-
-#   values = [
-#     var.remote_override_values
-#   ]
-# }
-
-## The local chart deployment
-resource "helm_release" "helm_local_deployment" {
-#   count         = var.enabled == true && var.remote_chart == false ? 1 : 0
-  name          = var.deployment_name
-  namespace     = var.deployment_environment
-  chart         = var.deployment_path
-  timeout       = var.timeout
-  recreate_pods = var.recreate_pods
-  version       = var.release_version
   values = [
     var.chart_override_values
   ]
-
-#   values = [
-#     trimspace(data.template_file.local_chart_values_template.0.rendered),
-#   ]
-}
-
-## The local chart values.yaml
-data "template_file" "local_chart_values_template" {
-#   count    = var.enabled == true && var.remote_chart == false ? 1 : 0
-  template = file("${var.deployment_path}/${var.values}")
-  vars     = local.template_all_values
-}
-
-terraform {
-  required_version = ">= 0.13.7"
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.5.1"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.2.3"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
-  }
 }
--- variable.tf
+++ variable.tf
@@ -1,66 +1,10 @@
-# variable"google_project_id"{
-#   description ="-(Required) Google cloud platform project id."
-# }
-
-# variable"google_credentials_json"{
-#   default     ="$HOME/terraform.json"
-#   description ="-(Optional) The google credentials file full path."
-# }
-
-# variable"google_bucket_name"{
-#   description ="-(Required) Google cloud platform project id."
-# }
-
-# variable"deployment_environment"{
-#   default     =""
-#   description ="-(Optional)"
-# }
-
-# variable"deployment_name"{
-#   description ="-(Required)"
-# }
-
-# variable"google_domain_name"{
-#   description ="-(Required)"
-# }
-
-# variable "ingress_controller" {
-#   type = map
-
-#   default = {
-#     version        = "4.0.19"
-#     enabled        = "true"
-#     chart_repo_url = "https://kubernetes.github.io/ingress-nginx"
-#   }
-
-#   description = "-(Optional) The Ingress controller map configuration."
-# }
-
-# variable "concourse" {
-#   type = map
-
-#   default = {
-#     version                = "17.0.37"
-#     enabled                = "true"
-#     local_user             = "admin"
-#     admin_password         = "password"
-#     postgres_username      = "admin"
-#     postgres_password      = "password"
-#     concourse_postgresql   = "concourse-postgresql"
-#     vault_token            = "s.mSNnbhGAqxK2ZbMasOQ91rIA"
-#     chart_repo_url         = "https://concourse-charts.storage.googleapis.com"
-#   }
-
-#   description = "-(Required) The Grafana map configuration."
-# }
+variable "deployment_name" {
+  description = "-(Required) The name of the deployment"
+}
 
 variable "deployment_endpoint" {
   description = "-(Optional) Endpoint for the application"
   default     = "example.local"
-}
-
-variable "deployment_name" {
-  description = "-(Required) The name of the deployment"
 }
 
 variable "deployment_environment" {
@@ -74,28 +18,6 @@ variable "deployment_path" {
 variable "release_version" {
   description = "-(Optional) Specify the exact chart version to install"
   default     = "0.1.0"
-}
-
-variable "remote_chart" {
-  type        = bool
-  default     = false
-  description = "-(Optional) For the remote charts set to <true>"
-}
-
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "-(Optional) deployment can be disabled or enabled by using this bool!"
-}
-
-variable "template_custom_vars" {
-  type        = map
-  default     = {}
-  description = "-(Optional) Local chart replace variables from values.yaml"
-}
-
-variable "trigger" {
-  default = "UUID"
 }
 
 variable "timeout" {
@@ -112,7 +34,7 @@ variable "values" {
   description = "-(Optional) Local chart <values.yaml> location"
 }
 
-variable "remote_override_values" {
+variable "chart_override_values" {
   default     = ""
   description = "-(Optional)"
 }
@@ -121,3 +43,25 @@ variable "chart_repo" {
   default     = ""
   description = "-(Optional) Provide the remote helm charts repository."
 }
+
+# variable "remote_chart" {
+#   type        = bool
+#   default     = false
+#   description = "-(Optional) For the remote charts set to <true>"
+# }
+
+# variable "enabled" {
+#   type        = bool
+#   default     = true
+#   description = "-(Optional) deployment can be disabled or enabled by using this bool!"
+# }
+
+# variable "template_custom_vars" {
+#   type        = map
+#   default     = {}
+#   description = "-(Optional) Local chart replace variables from values.yaml"
+# }
+
+# variable "trigger" {
+#   default = "UUID"
+# }
