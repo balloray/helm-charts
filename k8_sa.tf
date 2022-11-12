0a584ef7b5c24869cfc3bcfe134464dfbde5ec1a
rasheed balogun <balloray@gmail.com>
2022-11-12 03:33:16 +0000

    x

--- k8_sa.tf
+++ k8_sa.tf
@@ -1,14 +1,3 @@
-## Creating the secret to access GCP 
-resource "kubernetes_secret" "google_service_account" {
-  metadata {
-    name      = "google-service-account"
-  }
-  data = {
-    "credentials.json" = file(pathexpand("~/google-credentials.json"))
-  }
-  type = "generic"
-}
-
 resource "kubernetes_service_account" "common_service_account" {
   metadata {
     name      = "common-service-account"
@@ -23,4 +12,15 @@ resource "kubernetes_secret" "common_service_account_secret" {
   metadata {
     name      = "common-service-account-secret"
   }
+}
+
+## Creating the secret to access GCP 
+resource "kubernetes_secret" "google_service_account" {
+  metadata {
+    name      = "google-service-account"
+  }
+  data = {
+    "credentials.json" = file(pathexpand("~/google-credentials.json"))
+  }
+  type = "generic"
 }
